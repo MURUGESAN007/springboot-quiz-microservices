8f4c63c8f05fe2ae68195b8dfd63a31ff4bd3126
MURUGESAN007 <128365354+MURUGESAN007@users.noreply.github.com>
2026-05-28 00:17:53 +0530

    Add files via upload

---
+++ question-service/src/main/java/com/mrk7/question_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.mrk7.question_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value = ResourceNotFoundException.class)
+	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+		return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+}
